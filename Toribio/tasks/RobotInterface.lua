b8d55d61adf96bf8d18d338eae3288c21ff8d0ee
Andres <andresnebel@hotmail.com>
2013-08-27 18:51:53 -0300

    robot interface

--- Toribio/tasks/RobotInterface.lua
+++ Toribio/tasks/RobotInterface.lua
@@ -5,32 +5,43 @@ local devices = toribio.devices
 
 --Ejecuta en el kit robotico una funcion de un sensor con los parametros pasados
 M.Execute = function(sensor, func, params)
-
-end
+				--robado de bobot_server.lua
+				local device = devices[sensor]
+				local api_call=device[func];
+				if not api_call then return "missing call" end								
+				local ret = table.pack(pcall (api_call, unpack(parameters,4)))
+				local ok = ret[1]
+				if ok then 
+					return table.concat(ret, ',', 2)
+				else 
+					print ("error calling", table.concat(ret, ',', 2))
+				end
+			end
 
 --Enlista las funciones de los sensores y actuadores que poseen un mapeo en el config.
 --Si estas se encuentran disponibles en el kit robotico les asigna available en true
-M.ListSensorActuatorFunctions = function()
+M.ListSensorFunctions = function(device_type)
 	local file = xml.load("devices.xml")
 	local sensores = file:find("devices")
 	local ret = {}
 
 	for i=1, #sensores do
+		if (device_type = "any" or sensores[i].device_type = device_type) then
+			ret[i].alias = sensores[i].alias
+			--Esta en la lista de devices de bobot?
+			ret[i].available = (devices[sensores[i].name] ~= nil)
+			ret[i].functions = {}
 
-		ret[i].alias = sensores[i].alias
-		--Esta en la lista de devices de bobot?
-		ret[i].available = (devices[sensores[i].name] ~= nil)
-		ret[i].functions = {}
+			local functions = dev[i]:find("sensor")
+			for j=1, #functions do
 
-		local functions = dev[i]:find("sensor")
-		for j=1, #functions do
+				ret[i].functions[j].alias = functions[j].alias
+				ret[i].functions[j].params = functions[j].params
+				ret[i].functions[j].ret = functions[j].ret
+				--Esta en la lista de funciones del sensor en bobot?
+				ret[i].functions[j].available = (devices[sensores[i].name][functions[j].name] ~= nil)
 
-			ret[i].functions[j].alias = functions[j].alias
-			ret[i].functions[j].params = functions[j].params
-			ret[i].functions[j].ret = functions[j].ret
-			--Esta en la lista de funciones del sensor en bobot?
-			ret[i].functions[j].available = (devices[sensores[i].name][functions[j].name] ~= nil)
-
+			end
 		end
 	end	
 	return ret								
