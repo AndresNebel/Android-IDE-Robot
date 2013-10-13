35304b2a9b441a4c13bccc5f99b30569bcd00515
Andres <andresnebel@hotmail.com>
2013-10-13 11:14:32 -0200

    Version a medio mergear de blockly

---
+++ blockly now/language/es/_messages.js
@@ -0,0 +1,88 @@
+// Text strings (factored out to make multi-language easier).
+
+// Context menus.
+Blockly.MSG_REMOVE_COMMENT = 'Quitar Comentario';
+Blockly.MSG_ADD_COMMENT = 'Agregar Comentario';
+Blockly.MSG_EXTERNAL_INPUTS = 'Parametros Externos';
+Blockly.MSG_INLINE_INPUTS = 'Parametros en línea';
+Blockly.MSG_DELETE_BLOCK = 'Borrar Bloque';
+Blockly.MSG_DELETE_X_BLOCKS = 'Borrar %1 Bloque';
+Blockly.MSG_COLLAPSE_BLOCK = 'Minimizar Bloque';
+Blockly.MSG_EXPAND_BLOCK = 'Maximizar Bloque';
+Blockly.MSG_HELP = 'Ayuda';
+
+// Variable renaming.
+Blockly.MSG_CHANGE_VALUE_TITLE = 'Cambiar valor:';
+Blockly.MSG_NEW_VARIABLE = 'Nueva variable...';
+Blockly.MSG_NEW_VARIABLE_TITLE = 'Nombre de la nueva variable:';
+Blockly.MSG_RENAME_VARIABLE = 'Renombrar variable...';
+Blockly.MSG_RENAME_VARIABLE_TITLE = 'Renombrar todo "%1" variables con:';
+
+// Toolbox.
+Blockly.MSG_VARIABLE_CATEGORY = 'Variables';
+Blockly.MSG_PROCEDURE_CATEGORY = 'Procedimientos';
+
+// Mutator dialog.
+Blockly.MSG_MUTATOR_TOOLTIP = 'Editar este bloque';
+Blockly.MSG_MUTATOR_HEADER = 'Editor de bloque';
+Blockly.MSG_MUTATOR_CHANGE = 'Cambiar';
+Blockly.MSG_MUTATOR_CANCEL = 'Cancelar';
+
+// Logic Blocks
+Blockly.LANG_CATEGORY_LOGIC = 'Condiciones';
+Blockly.LANG_LOGIC_COMPARE_HELPURL = 'http://en.wikipedia.org/wiki/Inequality_(mathematics)';
+Blockly.LANG_LOGIC_COMPARE_TOOLTIP_EQ = 'Retorna verdadero si los dos parametros son iguales.';
+Blockly.LANG_LOGIC_COMPARE_TOOLTIP_NEQ = 'Retorna verdadero si los dos parametros no son iguales.';
+Blockly.LANG_LOGIC_COMPARE_TOOLTIP_LT = 'Retorna verdadero si el primer parametro es más pequeño\n' + 'que el segundo.';
+Blockly.LANG_LOGIC_COMPARE_TOOLTIP_LTE = 'Retorna verdadero si el primer parametro es más pequeño\n' + 'o igual que el segundo.';
+Blockly.LANG_LOGIC_COMPARE_TOOLTIP_GT = 'Retorna verdadero si el primer parametro es más grande\n' + 'que el segundo.';
+Blockly.LANG_LOGIC_COMPARE_TOOLTIP_GTE = 'Retorna verdadero si el primer parametro es más grande\n' + 'o igual que el segundo.';
+Blockly.LANG_LOGIC_OPERATION_HELPURL = 'http://code.google.com/p/blockly/wiki/And_Or';
+Blockly.LANG_LOGIC_OPERATION_AND = 'y';
+Blockly.LANG_LOGIC_OPERATION_OR = 'o';
+Blockly.LANG_LOGIC_OPERATION_TOOLTIP_AND = 'Retorna verdadero si los dos parametros son verdaderos.';
+Blockly.LANG_LOGIC_OPERATION_TOOLTIP_OR = 'Retorna verdadero si alguno de los dos parametros es verdadero.';
+Blockly.LANG_LOGIC_NEGATE_HELPURL = 'http://code.google.com/p/blockly/wiki/Not';
+Blockly.LANG_LOGIC_NEGATE_INPUT_NOT = 'no';
+Blockly.LANG_LOGIC_NEGATE_TOOLTIP_1 = 'Retorna verdadero si el parametro es falso.\n' + 'Retorna falso si el parametro es verdadero.';
+Blockly.LANG_LOGIC_BOOLEAN_HELPURL = 'http://code.google.com/p/blockly/wiki/True_False';
+Blockly.LANG_LOGIC_BOOLEAN_TRUE = 'verdadero';
+Blockly.LANG_LOGIC_BOOLEAN_FALSE = 'falso';
+Blockly.LANG_LOGIC_BOOLEAN_TOOLTIP_1 = 'Retorna verdadero o falso.'; 
+
+// Variables Blocks
+Blockly.LANG_VARIABLES_GET_HELPURL = 'http://en.wikipedia.org/wiki/Variable_(computer_science)';
+Blockly.LANG_VARIABLES_GET_TITLE_1 = 'obtener';
+Blockly.LANG_VARIABLES_GET_ITEM = 'elemento';
+Blockly.LANG_VARIABLES_GET_TOOLTIP_1 = 'Retorna el valor de esta variable.';
+
+Blockly.LANG_VARIABLES_SET_HELPURL = 'http://en.wikipedia.org/wiki/Variable_(computer_science)';
+Blockly.LANG_VARIABLES_SET_TITLE_1 = 'asignar';
+Blockly.LANG_VARIABLES_SET_ITEM = 'elemento';
+Blockly.LANG_VARIABLES_SET_TOOLTIP_1 = 'Asigna a esta variable el valor del parametro.';
+
+// Procedures Blocks
+Blockly.LANG_PROCEDURES_DEFNORETURN_HELPURL = 'http://en.wikipedia.org/wiki/Procedure_%28computer_science%29';
+Blockly.LANG_PROCEDURES_DEFNORETURN_PROCEDURE = 'procedimiento';
+Blockly.LANG_PROCEDURES_DEFNORETURN_DO = 'hacer';
+Blockly.LANG_PROCEDURES_DEFNORETURN_TOOLTIP_1 = 'Procedimiento sin valor de retorno.';
+
+Blockly.LANG_PROCEDURES_DEFRETURN_HELPURL = 'http://en.wikipedia.org/wiki/Procedure_%28computer_science%29';
+Blockly.LANG_PROCEDURES_DEFRETURN_PROCEDURE = 'procedimiento';
+Blockly.LANG_PROCEDURES_DEFRETURN_DO = 'hacer';
+Blockly.LANG_PROCEDURES_DEFRETURN_RETURN = 'retornar';
+Blockly.LANG_PROCEDURES_DEFRETURN_TOOLTIP_1 = 'Procedimiento con valor de retorno.';
+
+Blockly.LANG_PROCEDURES_CALLNORETURN_HELPURL = 'http://en.wikipedia.org/wiki/Procedure_%28computer_science%29';
+Blockly.LANG_PROCEDURES_CALLNORETURN_CALL = 'llamar';
+Blockly.LANG_PROCEDURES_CALLNORETURN_PROCEDURE = 'procedimiento';
+Blockly.LANG_PROCEDURES_CALLNORETURN_TOOLTIP_1 = 'Llama a un procedimiento que no retorna nada.';
+
+Blockly.LANG_PROCEDURES_CALLRETURN_HELPURL = 'http://en.wikipedia.org/wiki/Procedure_%28computer_science%29';
+Blockly.LANG_PROCEDURES_CALLRETURN_CALL = 'llamar';
+Blockly.LANG_PROCEDURES_CALLRETURN_PROCEDURE = 'procedimiento';
+Blockly.LANG_PROCEDURES_CALLRETURN_TOOLTIP_1 = 'Llama a un procedimiento que retorna un valor.';
+
+// Butia Blocks
+Blockly.LANG_CATEGORY_BUTIA = 'Butiá';
+Blockly.LANG_BUTIA_COMPARE_HELPURL = 'http://www.fing.edu.uy/inco/proyectos/butia/';
---
+++ blockly now/language/es/butia.js
@@ -0,0 +1,94 @@
+/**
+ * Visual Blocks Language
+ *
+ * Copyright 2012 Google Inc.
+ * http://code.google.com/p/blockly/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Power by: Yatay
+ * Due to the frequency of long strings, the 80-column wrap rule need not apply
+ * to language files.
+ */
+
+if (!Blockly.Language) Blockly.Language = {};
+
+Blockly.Language.butia_move = {
+	// Block for moving robot butiá forward or backwards.
+	category: Blockly.LANG_CATEGORY_BUTIA,
+	helpUrl: Blockly.LANG_BUTIA_COMPARE_HELPURL,
+	init: function() {
+		this.setColour(110);
+		this.appendTitle('mover');
+		var dropdown = new Blockly.FieldDropdown(this.DIRECTIONS);
+		this.appendTitle(dropdown, 'DIR');
+		this.setPreviousStatement(true);
+		this.setNextStatement(true);
+		this.setTooltip('Mueve el Butiá hacia adelante o atrás.');
+	}
+};
+
+Blockly.Language.butia_move.DIRECTIONS =
+	[['adelante', 'moveForward'], 
+	['atrás', 'moveBackward']];
+
+Blockly.Language.butia_turnLeftRight = {
+	// Block for turning robot butiá left or right.
+	category: Blockly.LANG_CATEGORY_BUTIA,
+	helpUrl: Blockly.LANG_BUTIA_COMPARE_HELPURL,
+	init: function() {
+		this.setColour(110);
+		this.appendTitle('girar');
+		var dropdown = new Blockly.FieldDropdown(this.DIRECTIONS);
+		this.appendTitle(dropdown, 'DIR');
+		this.setPreviousStatement(true);
+		this.setNextStatement(true);
+		this.setTooltip('Gira el Butiá hacia la derecha o izquierda.');
+	}
+};
+
+Blockly.Language.butia_turnLeftRight.DIRECTIONS =
+	[['izquierda', 'turnLeft'], 
+	['derecha', 'turnRight']];
+
+
+Blockly.Language.butia_stop = {
+  category: Blockly.LANG_CATEGORY_BUTIA,
+  init: function() {
+    this.setColour(0);
+	this.appendTitle('detener ')
+    this.setPreviousStatement(true);
+    this.setNextStatement(true);
+    // Assign 'this' to a variable for use in the tooltip closure below.
+    var thisBlock = this;
+    this.setTooltip(function() {   
+      return "Detener el Robot Butia";
+    });
+  }
+};
+
+Blockly.Language.butia_grey = {
+  category: Blockly.LANG_CATEGORY_BUTIA,
+  init: function() {
+    this.setColour(217);
+	this.appendTitle('sensor gris')
+    this.setOutput(true, Number);
+    // Assign 'this' to a variable for use in the tooltip closure below.
+    var thisBlock = this;
+    this.setTooltip(function() {   
+      return "El valor del sensor de grises el Robot Butia";
+    });
+  }
+};
