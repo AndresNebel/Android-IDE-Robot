4db82d40884b6cc4fbab86909c803c6f440bd382
Renzo <renzo.rozza@gmail.com>
2013-12-31 19:00:20 -0200

    moblie

--- Blockly/apps/yatay/js/common.js
+++ Blockly/apps/yatay/js/common.js
@@ -8,6 +8,110 @@ if (!Yatay.Common){
 } 
 
 /**
+ * Xml workspace code 
+ * @type {string}
+ */
+Yatay.Common.domCode = null; 
+
+/**
+ * Count workspace blocks
+ * @type {int}
+ */
+Yatay.Common.countBlocks = 0; 
+
+/**
+ * Attach autosave listener
+ */
+$(document).ready(function(){
+	$("#content_blocks").click(Yatay.Common.autoSave);
+});
+
+/**
+ * Autosave listener
+ */
+Yatay.Common.autoSave = function() {
+	if (Blockly.mainWorkspace != null) {
+		if (Yatay.Common.countBlocks != Blockly.mainWorkspace.getAllBlocks().length) {
+			var name = Blockly.mainWorkspace.getAllBlocks()[0].inputList[0].titleRow[0].text_;
+			if (name != "comportamiento") {
+				Yatay.Common.countBlocks = Blockly.mainWorkspace.getAllBlocks().length;
+				var xml = Blockly.Xml.workspaceToDom(Blockly.mainWorkspace);
+				var code = Blockly.Xml.domToText(xml);
+				Yatay.Common.saveTask(name, code);
+			}
+		}
+	}
+};
+
+/**
+ * Handle edit code click
+ */
+Yatay.Common.edit = function() {	
+	$('#code_editable').html(Blockly.Lua.workspaceToCode());
+	$('#code_modal').modal('show');
+};
+
+/**
+ * Show file chooser modal
+ */
+Yatay.Common.openFileChooser = function(){
+	$('#fchooser_modal').modal('show');
+	document.getElementById('file_input').addEventListener('change', Yatay.Common.readFile, false);
+};
+
+/**
+ * Read file, and load domCode
+ */
+Yatay.Common.readFile = function(evt) {
+	var f = evt.target.files[0];
+	if (f) {
+		var r = new FileReader();
+		r.onload = function(e) { 
+			Yatay.Common.domCode = Blockly.Xml.textToDom(e.target.result);  
+		}
+		r.readAsText(f);
+	} else { 
+		alert("Failed to load file");
+	}
+};
+
+/**
+ * Load code from xml
+ */
+Yatay.Common.fromXml = function() {
+	Blockly.Xml.domToWorkspace(Blockly.mainWorkspace, Yatay.Common.domCode);
+	$('#fchooser_modal').modal('hide');
+};
+
+/**
+ * Handle save click
+ */
+Yatay.Common.toXml = function() {
+	var xml = Blockly.Xml.workspaceToDom(Blockly.mainWorkspace);
+	var text = Blockly.Xml.domToText(xml);
+	var blob = new Blob([text], {type: "text/plain;charset=utf-8"});
+	var name = Blockly.mainWorkspace.getAllBlocks()[0].inputList[0].titleRow[0].text_;
+	saveAs(blob, name + ".xml");
+};
+
+/**
+ * Image click correction
+ */
+ Yatay.Common.imgClickCorrection = function() {
+	$(".imgclick").mousedown(function(){
+		var mrgtb = parseInt($(this).css("margin-top"));
+		var mrglf = parseInt($(this).css("margin-left"));
+		mrgtb=mrgtb+1; mrglf=mrglf+1;
+		$(this).css("margin-top",mrgtb+"px").css("margin-left",mrglf+"px");
+	}).mouseup(function(){
+		var mrgtb = parseInt($(this).css("margin-top"));
+		var mrglf = parseInt($(this).css("margin-left"));
+		mrgtb=mrgtb-1; mrglf=mrglf-1;
+		$(this).css("margin-top",mrgtb+"px").css("margin-left",mrglf+"px");
+	}); 
+};
+
+/**
  * Send task to server
  */
 Yatay.Common.sendTasks = function(code) {
@@ -23,7 +127,7 @@ Yatay.Common.sendTasks = function(code) {
 			//alert("failure");
 		}
 	});
-}
+};
 
 /**
  * Kill all tasks running
@@ -40,7 +144,7 @@ Yatay.Common.killTasks = function() {
 			//alert("failure");
 		}
 	});
-}
+};
 
 /**
  * Save current task
@@ -58,4 +162,4 @@ Yatay.Common.saveTask = function(name, code) {
 			//alert("failure");
 		}
 	});
-}
+};
--- Blockly/apps/yatay/js/tablet.js
+++ Blockly/apps/yatay/js/tablet.js
@@ -6,12 +6,6 @@
 if (!Yatay.Tablet){ 
 	Yatay.Tablet = {};
 } 
-
-/**
- * Xml workspace code 
- * @type {string}
- */
-Yatay.Tablet.domCode = null; 
  
 /**
  * Behaviours ready 
@@ -23,13 +17,7 @@ Yatay.Tablet.behaviours = [];
  * Test mode status
  * @type {bool}
  */
-Yatay.Tablet.testMode = false; 
-
-/**
- * Count workspace blocks
- * @type {int}
- */
-Yatay.Tablet.countBlocks = 0; 
+Yatay.Tablet.testMode = false;
 
 /**
  * Load tablet.html 
@@ -43,17 +31,7 @@ $(document).ready(function(){
 					"</ul>" +
 				"</div>");
 	list.appendTo($("#bx_ready"));
-	
-	$("#content_blocks").click(autoSave);
 });
-
-/**
- * Handle edit code click
- */
-function edit(){	
-	$('#code_editable').html(Blockly.Lua.workspaceToCode());
-	$('#code_modal').modal('show');
-};
 
 /**
  * Handle run click
@@ -65,6 +43,8 @@ function runTasks(){
 	$('#btn_run').toggle('slow');			
 	$('#btn_load').toggle('slow');
 	$('#btn_save').toggle('slow');		
+	$('#btn_bx_ready').toggle('slow');
+	$('#btn_trash').toggle('slow');
 	if($('#btn_edit').is(":visible")) {			
 		$('#btn_edit').toggle('slow');
 	}
@@ -81,6 +61,8 @@ function debug(){
 	$('#btn_run').toggle('slow');			
 	$('#btn_load').toggle('slow');
 	$('#btn_save').toggle('slow');
+	$('#btn_bx_ready').toggle('slow');
+	$('#btn_trash').toggle('slow');
 	if($('#btn_edit').is(":visible")) {			
 		$('#btn_edit').toggle('slow');
 	}
@@ -106,6 +88,7 @@ function stop(){
 	$('#btn_save').toggle('slow');
 	$('#btn_stop').toggle('slow');
 	$('#btn_bx_ready').toggle('slow');	
+	$('#btn_trash').toggle('slow');
 };
 
 /**
@@ -124,52 +107,11 @@ function robotest(){
 	$('#btn_load').toggle('slow');
 	$('#btn_save').toggle('slow');
 	$('#btn_bx_ready').toggle('slow');
+	$('#btn_trash').toggle('slow');
 	if($('#btn_edit').is(":visible")) {			
 		$('#btn_edit').toggle('slow');
 	}
 	$('#btn_stop').toggle();
-};
-
-/**
- * Handle save click
- */
-function toXml() {
-	var xml = Blockly.Xml.workspaceToDom(Blockly.mainWorkspace);
-	var text = Blockly.Xml.domToText(xml);
-	var blob = new Blob([text], {type: "text/plain;charset=utf-8"});
-	saveAs(blob, "yatay.xml");
-};
-
-/**
- * Read YataY file, and load domCode
- */
-function readFile(evt) {
-	var f = evt.target.files[0];
-	if (f) {
-		var r = new FileReader();
-		r.onload = function(e) { 
-			Yatay.Tablet.domCode = Blockly.Xml.textToDom(e.target.result);  
-		}
-		r.readAsText(f);
-	} else { 
-		alert("Failed to load file");
-	}
-};
-
-/**
- * Show file chooser modal
- */
-function openFileChooser(){
-	$('#fchooser_modal').modal('show');
-	document.getElementById('file_input').addEventListener('change', readFile, false);
-};
-
-/**
- * Load code from xml
- */
-function fromXml() {
-	Blockly.Xml.domToWorkspace(Blockly.mainWorkspace, Yatay.Tablet.domCode);
-	$('#fchooser_modal').modal('hide');
 };
 
 /**
@@ -204,23 +146,6 @@ var resizeTextarea = function() {
 };
 
 $('textarea').keydown(resizeTextarea).keyup(resizeTextarea).change(resizeTextarea).focus(resizeTextarea);
-
-/**
- * Image click correction
- */
- function imgClickCorrection() {
-	$(".imgclick").mousedown(function(){
-		var mrgtb = parseInt($(this).css("margin-top"));
-		var mrglf = parseInt($(this).css("margin-left"));
-		mrgtb=mrgtb+1; mrglf=mrglf+1;
-		$(this).css("margin-top",mrgtb+"px").css("margin-left",mrglf+"px");
-	}).mouseup(function(){
-		var mrgtb = parseInt($(this).css("margin-top"));
-		var mrglf = parseInt($(this).css("margin-left"));
-		mrgtb=mrgtb-1; mrglf=mrglf-1;
-		$(this).css("margin-top",mrgtb+"px").css("margin-left",mrglf+"px");
-	}); 
-};
 
 /**
  * Set behaviour as ready
@@ -266,21 +191,6 @@ function bxToWorkspace() {
 				bxReady();
 			}
 			Blockly.Xml.domToWorkspace(Blockly.mainWorkspace, code);
-		}
-	}
-};
-
-/**
- * Autosave Listener
- */
-function autoSave(){
-	if (Blockly.mainWorkspace != null) {
-		if (Yatay.Tablet.countBlocks != Blockly.mainWorkspace.getAllBlocks().length) {
-			Yatay.Tablet.countBlocks = Blockly.mainWorkspace.getAllBlocks().length;
-			var xml = Blockly.Xml.workspaceToDom(Blockly.mainWorkspace);
-			var code = Blockly.Xml.domToText(xml);
-			var name = Blockly.mainWorkspace.getAllBlocks()[0].inputList[0].titleRow[0].text_;
-			Yatay.Common.saveTask(name, code);
 		}
 	}
 };
